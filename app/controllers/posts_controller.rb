708ea885f86da1954d19029df33ba30ef62884bb
domanhtien2011 <domanhtien2011@gmail.com>
2015-12-21 18:57:32 +0700

    Add comment to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,12 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-
   def index
     @posts = Post.all.order("created_at ASC")
+  end
+
+  def show
+    @comments = Comment.where(post_id: @post)
   end
 
   def new
